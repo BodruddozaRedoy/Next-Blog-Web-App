efb8a2c89deb098a446bdf73d4e4cb721acf4025
bodruddozaredoy <bodruddozaredoy@gmail.com>
2025-09-29 06:27:56 +0600

    Updated latest code

--- src/app/(public)/blogs/[blogId]/page.tsx
+++ src/app/(public)/blogs/[blogId]/page.tsx
@@ -1,18 +1,45 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import BlogDetailsCard from "@/components/modules/Blogs/BlogDetailsCard";
+import { getBlogById } from "@/services/PostService";
 import React from "react";
 
-export default async function BlogDetailsPage({
+export const generateStaticParams = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post`);
+  const { data: blogs } = await res.json();
+
+  return blogs.slice(0, 2).map((blog: any) => ({
+    blogId: String(blog.id),
+  }));
+};
+
+export const generateMetadata = async ({
   params,
 }: {
-  params: { blogId: string };
-}) {
-    const { blogId } = params;
-    console.log(blogId);
+  params: Promise<{ blogId: string }>;
+}) => {
+  const { blogId } = await params;
+  const blog = await getBlogById(blogId);
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post/${blogId}`, {
-    cache: "no-store", // optional: prevents stale cache
-  });
-  const { data: post } = await res.json();
-  console.log(post);
+  return {
+    title: blog?.title,
+    description: blog?.content,
+  };
+};
 
-  return <div>{post?.id}</div>;
-}
+const BlogDetailsPage = async ({
+  params,
+}: {
+  params: Promise<{ blogId: string }>;
+}) => {
+  const { blogId } = await params;
+
+  const blog = await getBlogById(blogId);
+
+  return (
+    <div className="py-30 px-4 max-w-7xl mx-auto">
+      <BlogDetailsCard blog={blog} />
+    </div>
+  );
+};
+
+export default BlogDetailsPage;

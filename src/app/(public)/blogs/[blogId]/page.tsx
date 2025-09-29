6177b9367ebeece2bff7c3da887598f0ddc0ad27
bodruddozaredoy <bodruddozaredoy@gmail.com>
2025-09-30 05:31:46 +0600

    skip dynamic blogs

--- src/app/(public)/blogs/[blogId]/page.tsx
+++ src/app/(public)/blogs/[blogId]/page.tsx
@@ -3,14 +3,14 @@ import BlogDetailsCard from "@/components/modules/Blogs/BlogDetailsCard";
 import { getBlogById } from "@/services/PostService";
 import React from "react";
 
-export const generateStaticParams = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post`);
-  const { data: blogs } = await res.json();
+// export const generateStaticParams = async () => {
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/post`);
+//   const { data: blogs } = await res.json();
 
-  return blogs.slice(0, 2).map((blog: any) => ({
-    blogId: String(blog.id),
-  }));
-};
+//   return blogs.slice(0, 2).map((blog: any) => ({
+//     blogId: String(blog.id),
+//   }));
+// };
 
 export const generateMetadata = async ({
   params,
